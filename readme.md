424b4673bcf04c97d7cfc037d1f66f69bd5b43b3
chenxiuli <chenxiuli@bwton.com>
2020-02-21 21:15:00 +0800

    1、services 包封装成功和失败的响应方法 2、处理前台静态页面 3、完成前台借口

--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 - 确保本地已经安装了 go 环境，且支持 go modules
 - 确保本地安装了 gcc
+
+- 前端页面，请将 vue 项目打包后，分别放到 templates/html 下，资源文件则放在 template/static 下
--- services/router.go
+++ services/router.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"gin-blog/interfaces"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,26 +10,53 @@ var RouterEngine *gin.Engine
 var pageNum uint64
 var H interfaces.Helper
 
+// 成功响应
+func Success(c *gin.Context, data gin.H) {
+	res := map[string]interface{}{
+		"code":    "1",
+		"message": "ok",
+	}
+	res["data"] = data
+
+	c.JSON(200, res)
+}
+
+// 失败响应
+func Failed(c *gin.Context, code int, err error) {
+	res := map[string]interface{}{
+		"code":    "-1",
+		"message": err.Error(),
+		"data":    make(gin.H),
+	}
+
+	c.JSON(code, res)
+}
+
 // 注册路由
 func RegisterRouter(r *gin.Engine, h interfaces.Helper) *gin.Engine {
+	// 包范围内初始化辅助函数包
 	H = h
 
+	// 包范围内初始化每页条数
 	numStr := H.GetConfig("page_num").(int)
 	pageNum = uint64(numStr)
 
+	// 包范围内初始化引擎
 	RouterEngine = r
 	r.NoRoute(Handle404)
 
-	// 静态资源
+	// 静态资源服务
 	{
-		assetPath := H.GetConfig("assets_path").(string)
-		r.Static("/asset", assetPath)
+		r.Static("/static", H.GetConfig("static_path").(string))
 	}
 
-	// 前台路由
+	r.GET("/", IndexHtml)
+	//r.GET("/admin", AdminHtml)
+
+	// 前台路由 API
 	{
 		// 首页
-		r.GET("/", Index)
+		r.GET("/index", Index)
 		// 专辑列表
 		r.GET("/categories", Categories)
 		// 文章列表
@@ -41,7 +69,7 @@ func RegisterRouter(r *gin.Engine, h interfaces.Helper) *gin.Engine {
 
 	// 后台路由
 	{
-		// BaseAuth 中间件
+		// Auth 中间件
 
 		// 单文件上传
 
